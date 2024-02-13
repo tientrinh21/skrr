6d00c15d5407d32f86ac02d91856edb7b3a64162
tientrinh21 <tqtien.a11619@gmail.com>
2024-02-14 03:43:39 +0900

    Fix mobile UI horizontal scroll behavior

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,6 +14,8 @@
 
 <svelte:head>
 	<title>SKrr - {title}</title>
+	<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1.0" />
+	<meta name="apple-mobile-web-app-capable" content="yes" />
 </svelte:head>
 
 <ModeWatcher defaultMode="light" />
